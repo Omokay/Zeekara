c418aaeb6ea781fbe4096e6602c05313f511dcb2
Omokay <chuku.omoke@gmail.com>
2020-04-20 02:46:14 +0100

    add shop component with collections

--- src/components/Collections/collections.component.jsx
+++ src/components/Collections/collections.component.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import './collections.style.scss';
 
+import CollectionItem from '../CollectionItem/collection-item.component';
+
 
 const Collections = ({ title, items }) => {
     return (
-        <div className='collection-preview"'>
+        <div className='collection-preview'>
             <h1 className='title'>{title.toUpperCase()}</h1>
             <div className='preview'>
-                {items.filter((item, idx) => idx < 4).map(item => (
-                        <div key={item.id}>{item.name}</div>
+                {items.filter((item, idx) => idx < 4)
+                .map(({id, ...otherItemProps}) => (
+                        <CollectionItem key={id} {...otherItemProps} />
                     ))}
             </div>
         </div>
