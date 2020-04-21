05f8124ef5d1847db1021a4ee79473b4ec37eca4
Omokay <chuku.omoke@gmail.com>
2020-04-21 01:57:50 +0100

    add signin ui

---
+++ src/components/Signin/signin.component.jsx
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import './signin.style.scss';
+
+import FormInput from '../../components/FormInput/form-input.component';
+import CustomButton from '../../components/CustomButton/custom-button.component';
+
+
+class Signin extends Component {
+    constructor(props) {
+        super(props);
+
+
+        this.state = {
+            email: '',
+            password: ''
+        };
+    }
+
+    handleSubmit = event => {
+        event.preventDefault();
+
+        this.setState({ email: '', password: '' });
+    };
+
+    handleChange = event => {
+        const { value, name } = event.target;
+
+        this.setState({ [name]: value });
+    };
+
+    
+    render() {
+        
+        return (
+            <div className='sign-in'>
+                <h2>I already have an account</h2>
+                <span>Sign in with your email and password</span>
+
+                <form onSubmit={this.handleSubmit}>
+                    <FormInput
+                        name='email'
+                        type='email'
+                        handleChange={this.handleChange}
+                        value={this.state.email}
+                        label='Email'
+                        required
+                    />
+                    <FormInput
+                        name='password'
+                        type='password'
+                        value={this.state.password}
+                        handleChange={this.handleChange}
+                        label='P assword'
+                        required
+                    />
+                    <CustomButton type='submit'>Sign in</CustomButton>
+                </form>
+            </div>
+        )
+    }
+}
+
+
+export default Signin;
---
+++ src/pages/Signin-Signup/signin-signup.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './signin-signup.style.scss';
+import Signin from '../../components/Signin/signin.component';
+
+
+const SigninSignup = () => {
+    return (
+        <div className='signin-and-signup'>
+            <Signin />
+        </div>
+    )
+}
+
+export default SigninSignup;
