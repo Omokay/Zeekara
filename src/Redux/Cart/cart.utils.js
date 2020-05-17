d4afa360b34fd35ee973e5ee16eafed7af8df95e
Omokay <chuku.omoke@gmail.com>
2020-05-17 01:46:30 +0100

    debug quantity not incrementing in add item to cart action

--- src/Redux/Cart/cart.utils.js
+++ src/Redux/Cart/cart.utils.js
@@ -1,12 +1,14 @@
  export const addItemToCart = (cartItems, cartItemToAdd) => {
-    const existingCart = cartItems.find(cartItem => cartItem.id === cartItemToAdd.id);
-    if (existingCart) {
-        return (cartItems.map(cartItem =>
-            cartItem.id === cartItemToAdd
-                ? { ...cartItem, quantity: cartItem.quantity + 1 } : cartItem
-        )
+     const existingCartItem = cartItems.find(
+         cartItem => cartItem.id === cartItemToAdd.id
+     );
+     if (existingCartItem) {
+        return cartItems.map(cartItem =>
+            cartItem.id === cartItemToAdd.id
+                ? { ...cartItem, quantity: cartItem.quantity + 1 }
+                : cartItem
         );
     }
 
-    return [...cartItems, {...cartItemToAdd, quantity: 1}]
+     return [...cartItems, { ...cartItemToAdd, quantity: 0 }];
 }; 
