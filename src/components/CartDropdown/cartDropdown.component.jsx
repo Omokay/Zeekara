33f27e1259acdf76c5db5ca77aa7206d5550af02
Omokay <chuku.omoke@gmail.com>
2020-05-24 16:02:39 +0100

    update cart selectors with reselect using memoization

--- src/components/CartDropdown/cartDropdown.component.jsx
+++ src/components/CartDropdown/cartDropdown.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import CustomButton from '../../components/CustomButton/custom-button.component';
 import CartItem from '../CartItem/cartItem.component';
+import { selectCartItems } from '../../Redux/Cart/cart.selector';
 import { connect } from 'react-redux';
 
 import './cartDropdown.styles.scss';
@@ -10,16 +11,21 @@ const Cart = ({ cartItems }) => (
     <div className='cart-dropdown'>
         <div className='cart-items'>
             {cartItems.map(cartItem => (
-                <CartItem id={cartItem.id} item={cartItem} />
+                <CartItem key={cartItem.id} item={cartItem} />
             ))}
             <CustomButton>Checkout</CustomButton>
         </div>
     </div>
 );
 
+// Selector before memoization
+// const mapStateToProps = ({ cart: { cartItems } }) => ({
+//     cartItems
+// });
 
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-    cartItems
+// Using Memoization
+const mapStateToProps = (state) => ({
+    cartItems: selectCartItems(state)
 });
 
 export default connect(mapStateToProps)(Cart);
