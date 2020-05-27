c27a4e8a4611d2d20aad7e77691798691b359089
Omokay <chuku.omoke@gmail.com>
2020-05-27 22:25:41 +0100

    added redux persist and set config to handle cartItems

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,4 +1,5 @@
 import { createStore, applyMiddleware } from 'redux';
+import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
 
 import './root-reducer';
@@ -7,7 +8,8 @@ import rootReducer from './root-reducer';
 
 const middleware = [logger];
 
-const store = createStore(rootReducer, applyMiddleware(...middleware)); 
+export const store = createStore(rootReducer, applyMiddleware(...middleware)); 
 
+export const persistor = persistStore(store);
 
-export default store;
+export default {store, persistor};
--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import App from './App';
-import store from './Redux/store';
+import {store, persistor} from './Redux/store';
 
 import './index.css';
 
@@ -12,7 +13,9 @@ ReactDOM.render(
   // Pass store in Provider tag to get the store context all over the application
     <Provider store={store}>
       <BrowserRouter>
+      <PersistGate persistor={persistor}>
         <App />
+        </PersistGate>
       </BrowserRouter>
     </Provider>,
   document.getElementById('root')
